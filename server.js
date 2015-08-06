2f831448f709c3f5c435fd15ada8b798e8339ecf
EricScottRichards <ericscottrichards@gmail.com>
2015-08-06 15:59:00 -0600

    NOW it's finished.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require ('http');
 var messages = ['shazam', 'aw yeah', 'ok'];
-var onRequest = function(req,res){
+
+http.createServer(function(req,res){
 	if(req.method === 'GET'){
 		res.writeHead(200, {
 			'Connection': 'close',
@@ -9,15 +10,34 @@ var onRequest = function(req,res){
 		});
 		res.end(JSON.stringify(messages))
 	}
-	if(req.method == 'POST'){
+	if(req.method === 'POST'){
 		var postData = '';
 		req.on('data', function(chunk){
 			postData += chunk.toString();
 		});
 		req.on('end', function() {
 			console.log("Got POST data:");
-			console.log(JSON.parse(postData))
+			console.log(JSON.parse(postData));
+			var msg = JSON.parse(postData);
+			messages.push(msg);
+      res.writeHead(200, {
+        'Connection': 'close',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      });
+      res.end(JSON.stringify(messages))
 		})
 	}
-}
-http.createServer(onRequest).listen(9090);
+	if(req.method === 'OPTIONS'){
+		res.writeHead(200, {
+			'Connection': 'close',
+      'Content-Type': 'application/json',
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+			'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+		});
+		res.end("{}")
+	}
+
+}).listen(9090);
+
